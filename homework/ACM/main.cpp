9002875c248a8cc3ba4b47fae873044431867a6f
muzea <mr.muzea@gmail.com>
2015-10-13 19:48:11 +0800

    更新第二次作业

--- homework/ACM/main.cpp
+++ homework/ACM/main.cpp
@@ -19,41 +19,41 @@ using namespace std;
 int main() {
 
 	P_MARK(1,1);
-	cout << P1::recursive(4) << endl;
-	//cout << P1::tail_recursive(4) << endl;
-	//cout << P1::formula_of_general_term(4) << endl;
+	cout << P1_1::recursive(4) << endl;
+	//cout << P1_1::tail_recursive(4) << endl;
+	//cout << P1_1::formula_of_general_term(4) << endl;
 
 	P_MARK(1,2);
-	cout << P2::solve(10) << endl;
+	cout << P1_2::solve(10) << endl;
 
 
 	int Arr[5] = { 1,3,5,7,8 };
 	P_MARK(1,3);
-	cout << P3::find(Arr, 5) << endl;
+	cout << P1_3::find(Arr, 5) << endl;
 
 	int i = 0, j = 5;
 	P_MARK(1,4);
-	P4::find(Arr, sizeof Arr, i, j, 5);
+	P1_4::find(Arr, sizeof Arr, i, j, 5);
 	cout << i << "  " << j << endl;
 
 	P_MARK(1,5);
-	P5::solve();
+	P1_5::solve();
 
 	P_MARK(1,6);
-	P6::solve();
-	//P6::printNarcissistic<100,999>::print();
+	P1_6::solve();
+	//P1_6::printNarcissistic<100,999>::print();
 
 	P_MARK(1,7);
-	P7::solve();
+	P1_7::solve();
 
 	P_MARK(1,8);
-	P8::solve();
+	P1_8::solve();
 
 	P_MARK(1,9);
-	P9::solve();
+	P1_9::solve();
 
 	P_MARK(1,10);
-	P10::solve();
+	P1_10::solve();
 	
 
 	P_MARK(2, 1);
